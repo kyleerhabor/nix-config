d71f01e538322a83225a90bd8b84cac298c04c3b
Kyle Erhabor <kyleerhabor@gmail.com>
2026-07-05 15:22:16 -0400

    Simplify gitignore file path

--- home/kyleerhabor.nix
+++ home/kyleerhabor.nix
@@ -1,13 +1,11 @@
-{ pkgs, ... }: let
-  gitignore = pkgs.writeText "global-gitignore" (builtins.readFile ./kyleerhabor/resources/git/ignore);
-in {
+{ ... }: {
   # Enable Git integration.
   programs.git.enable = true;
 
   # Configuration written to $XDG_CONFIG_HOME/git/config. See git-config(1) for details.
   programs.git.settings.user.name = "Kyle Erhabor";
   programs.git.settings.user.email = "kyleerhabor@gmail.com";
-  programs.git.settings.core.excludesFile = "${gitignore}";
+  programs.git.settings.core.excludesFile = "${./kyleerhabor/resources/git/ignore}";
 
   # Enable Nushell integration.
   programs.nushell.enable = true;
